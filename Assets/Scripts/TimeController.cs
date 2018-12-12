7b21588c865658634e883ea8c27caefe98fb42cd
Nikolay Ivanov <niki.ivanov98@gmail.com>
2018-12-12 18:00:10 +0100

    Added sun and lamps lighting at times of day

---
+++ Assets/Scripts/TimeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeController : MonoBehaviour
+{
+	public GameObject[] LampLights;
+	public GameObject SunRays;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// We get the user's system date format and convert it
+		// to a 24 hour format so that the code can work with it better.
+		string systemDateFormat = DateTime.Now.ToString("HH");
+		int currentHour24 = Int32.Parse(systemDateFormat);
+		
+		// Updating the light rays and lamps depending on the time of day
+		if (currentHour24 > 8 && currentHour24 < 18) // Day Time
+		{
+			foreach (GameObject lamp in LampLights)
+			{
+				lamp.SetActive(false);
+			}
+			
+			SunRays.GetComponent<Light>().color = new Color32(0x0FF, 0x0C4, 0x070, 0x00);
+		}
+		else // Night Time
+		{
+			foreach (GameObject lamp in LampLights)
+			{
+				lamp.SetActive(true);
+			}
+			
+			SunRays.GetComponent<Light>().color = new Color32(0x000, 0x009, 0x076, 0x00);
+		}
+
+	}
+}
