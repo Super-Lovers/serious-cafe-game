8ed3991be443d88c55529f79cfc4cddd7ce7694d
Nikolay Ivanov <niki.ivanov98@gmail.com>
2019-01-06 02:35:09 +0100

    Implemented pause menu with volume settings

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -1,12 +1,96 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class ButtonController : MonoBehaviour {
     public Material HoveredButton;
+    public GameObject EscapeMenu;
+    public GameObject OptionsMenu;
+    public static bool IsEscapeMenuVisible = false;
+    private bool _isOptionsMenuVisible = false;
+
+    public AudioSource RainAudio;
 
     public void HighlightButton()
     {
         GetComponent<MeshRenderer>().material = HoveredButton;
+    }
+
+    public void ToggleEscapeMenu()
+    {
+        PlayerController._isEscapeButtonPressed = !PlayerController._isEscapeButtonPressed;
+        IsEscapeMenuVisible = !IsEscapeMenuVisible;
+        EscapeMenu.SetActive(IsEscapeMenuVisible);
+    }
+
+    public void BackToMainMenu()
+    {
+        Debug.Log("Back to Main Menu");
+    }
+
+    public void ShowOptionsMenu()
+    {
+        _isOptionsMenuVisible = !_isOptionsMenuVisible;
+        OptionsMenu.SetActive(_isOptionsMenuVisible);
+    }
+
+    public void UpdateVolume()
+    {
+        string nameOfClickedButton = EventSystem.current.currentSelectedGameObject
+            .transform.parent.name;
+        string nameOfVolumeSetting = EventSystem.current.currentSelectedGameObject
+            .name;
+
+        GameObject[] customers = 
+            GameObject.FindGameObjectsWithTag("Customer");
+
+        switch (nameOfClickedButton)
+        {
+            case "Rain Volume":
+                if (nameOfVolumeSetting == "Increase")
+                {
+                    RainAudio.volume += 0.1f;
+                }
+                else
+                {
+                    RainAudio.volume -= 0.1f;
+                }
+                break;
+            case "Dialogue Volume":
+                if (nameOfVolumeSetting == "Increase")
+                {
+                    foreach (GameObject customer in customers)
+                    {
+                        customer.GetComponent<AudioSource>().volume += 0.1f;
+                    }
+                }
+                else
+                {
+                    foreach (GameObject customer in customers)
+                    {
+                        customer.GetComponent<AudioSource>().volume -= 0.1f;
+                    }
+                }
+                break;
+            case "Master Volume":
+                if (nameOfVolumeSetting == "Increase")
+                {
+                    RainAudio.volume += 0.1f;
+                    foreach (GameObject customer in customers)
+                    {
+                        customer.GetComponent<AudioSource>().volume += 0.1f;
+                    }
+                }
+                else
+                {
+                    RainAudio.volume -= 0.1f;
+                    foreach (GameObject customer in customers)
+                    {
+                        customer.GetComponent<AudioSource>().volume -= 0.1f;
+                    }
+                }
+                break;
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,33 +17,48 @@ public class PlayerController : MonoBehaviour
 	private float _mouseX = 0f;
 	private float _mouseY = 0f;
 
+    private GameObject _buttonsController;
+    public static bool _isEscapeButtonPressed = false;
+
 	void Start ()
 	{
 		_playerCamera = GetComponentInChildren<Camera>();
 		_charController = GetComponent<CharacterController>();
+        _buttonsController = GameObject.FindGameObjectWithTag("ButtonsController");
 	}
 	
 	void Update ()
 	{
-		// The played must only move to the sides
-		_charController.Move(new Vector3(Input.GetAxis("Horizontal") * MovementSpeed, 0, 0));
-		
-		// The mouse cursor has to be centered on the screen so that he can interact
-		// with objects by looking at them first.
-		Cursor.lockState = CursorLockMode.Locked;
-		
-		// Here we change AND store the value of the x position, so that
-		// we can change it dynamically and use the stored values so when we
-		// move the camera, it moves relative to its last stored position.
-		_mouseX += Input.GetAxis("Mouse X");
-		// Plus or Minus means the direction of the mouse that its going to increment
-		// as its value relative to the movement position on the screen.
-		_mouseY -= Input.GetAxis("Mouse Y");
-		float clampedHorizontalRotation = Mathf.Clamp(_mouseX * CameraSensitivity, -70, 70);
-		float clampedVerticalRotation = Mathf.Clamp(_mouseY * CameraSensitivity, -30, 30);
-		
-		_playerCamera.transform.localEulerAngles = new Vector3(clampedVerticalRotation, clampedHorizontalRotation, 0);
-	}
+		if (_isEscapeButtonPressed == false)
+        {
+            // The played must only move to the sides
+            _charController.Move(new Vector3(Input.GetAxis("Horizontal") * MovementSpeed, 0, 0));
+
+            // The mouse cursor has to be centered on the screen so that he can interact
+            // with objects by looking at them first.
+            Cursor.lockState = CursorLockMode.Locked;
+
+            // Here we change AND store the value of the x position, so that
+            // we can change it dynamically and use the stored values so when we
+            // move the camera, it moves relative to its last stored position.
+            _mouseX += Input.GetAxis("Mouse X");
+            // Plus or Minus means the direction of the mouse that its going to increment
+            // as its value relative to the movement position on the screen.
+            _mouseY -= Input.GetAxis("Mouse Y");
+            float clampedHorizontalRotation = Mathf.Clamp(_mouseX * CameraSensitivity, -70, 70);
+            float clampedVerticalRotation = Mathf.Clamp(_mouseY * CameraSensitivity, -30, 30);
+
+            _playerCamera.transform.localEulerAngles = new Vector3(clampedVerticalRotation, clampedHorizontalRotation, 0);
+        } else
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            _buttonsController.GetComponent<ButtonController>().ToggleEscapeMenu();
+        }
+    }
 
 	private void OnGUI()
 	{
